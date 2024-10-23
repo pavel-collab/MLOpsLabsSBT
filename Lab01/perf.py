65f30d829c44086c472c207460b88eb89e8c982e
Filippenko Pavel <filippenko.ps@phystech.edu>
2024-10-23 23:51:50 +0300

    fix random vector generation in perf file

--- Lab01/perf.py
+++ Lab01/perf.py
@@ -12,10 +12,15 @@ def test_timings(func: callable, *args):
     end_time = time.time()
     return round(end_time - start_time, 5)
 
+def gen_valid_random_vec(length: int):
+    vec = np.random.rand(length)
+    while np.linalg.norm(vec) == 0:
+        vec = np.random.rand(length)
+    return vec
 
 if __name__ == "__main__":
     for length in [5, 25, 125, 625, 3125, 15625, 78125]:
-        vec1 = np.random.rand(length)
-        vec2 = np.random.rand(length)
+        vec1 = gen_valid_random_vec(length)
+        vec2 = gen_valid_random_vec(length)
         print("Cosin distance (Pure C++),     size={0}: {1} seconds".format(length, test_timings(CosinDistance.get_cos_distance, vec1, vec2)))
         print("Cosin distance (Python scipy), size={0}: {1} seconds\n".format(length, test_timings(distance.cosine, vec1, vec2)))
