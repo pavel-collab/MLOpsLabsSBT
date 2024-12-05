b00cd2d0c8d2528e1690fe6fe0f4f3b19f4112cc
Filippenko Pavel <filippenko.ps@phystech.edu>
2024-12-05 14:43:12 +0300

    fix client

--- Lab03/client.py
+++ Lab03/client.py
@@ -9,7 +9,7 @@ PORT = "8000"
 
 @hydra.main(config_path="./conf", config_name="config", version_base="1.2")
 def run_triton_inderence(cfg):
-    lables = {0: "unacceptable", 1: "acceptable"}
+    lables = ["unacceptable", "acceptable"]
 
     data = MyDataModule(model_name=cfg.model.name,
                         tokenizer_name=cfg.model.tokenizer,
@@ -21,14 +21,19 @@ def run_triton_inderence(cfg):
 
     input_batch = next(iter(data.train_dataloader()))
 
-    input_ids = httpclient.InferInput("input_ids", input_batch["input_ids"][0].reshape(1, -1).shape, "INT32")
-    attention_mask = httpclient.InferInput("attention_mask", input_batch["attention_mask"][0].reshape(1, -1).shape, "INT32")
+    decoded_sentence = data.tokenizer.decode(input_batch['input_ids'][0], skip_special_tokens=True)
+    true_label = input_batch['label'][0]
+    print(f"true label is {true_label}")
+    print(f"sentence is {decoded_sentence}")
+
+    input_ids = httpclient.InferInput("input_ids", input_batch["input_ids"][0].reshape(1, -1).shape, "INT64")
+    attention_mask = httpclient.InferInput("attention_mask", input_batch["attention_mask"][0].reshape(1, -1).shape, "INT64")
 
     triton_server_url = f"localhost:{PORT}"
     client = httpclient.InferenceServerClient(url=triton_server_url)
 
-    input_ids.set_data_from_numpy(np.array(input_batch["input_ids"][0].unsqueeze(0)).astype('int32')) #TODO: изучить структуру входного аргумента, подумать, как можно красивее конвертнуть в numpy array
-    attention_mask.set_data_from_numpy(np.array(input_batch["attention_mask"][0].unsqueeze(0)).astype('int32'))
+    input_ids.set_data_from_numpy(np.array(input_batch["input_ids"][0].reshape(1, -1)).astype('int64'))
+    attention_mask.set_data_from_numpy(np.array(input_batch["attention_mask"][0].reshape(1, -1)).astype('int64'))
 
     output = httpclient.InferRequestedOutput("output")
 
@@ -41,7 +46,7 @@ def run_triton_inderence(cfg):
     logits = response.as_numpy("output")
     scores = softmax(logits)[0]
     predictions = []
-    for score, label in zip(scores, lables.values()):
+    for score, label in zip(scores, lables):
         predictions.append({"label": label, "score": score})
     print(predictions)
 
