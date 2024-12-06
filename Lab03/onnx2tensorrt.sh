f9a4ad66b30672334a35e396bdc8e6b42db65f24
Pavel Filippenko <rubikpf2002@mail.ru>
2024-12-06 16:04:56 +0300

    fix converting to tensorrt

--- Lab03/onnx2tensorrt.sh
+++ Lab03/onnx2tensorrt.sh
@@ -2,40 +2,23 @@
 
 trtexec --onnx=model.onnx \
 --saveEngine=model_fp16.plan \
---minShapes=input:1 \
---optShapes=input:8 \
---maxShapes=input:8 \
---inputIOFormats=fp32:chw \
 --fp16 \
 --profilingVerbosity=detailed \
 --builderOptimizationLevel=5
 
 trtexec --onnx=model.onnx \
 --saveEngine=model_fp32.plan \
---minShapes=input:1 \
---optShapes=input:8 \
---maxShapes=input:8 \
---inputIOFormats=fp32:chw \
---fp32 \
 --profilingVerbosity=detailed \
 --builderOptimizationLevel=5
 
 trtexec --onnx=model.onnx \
 --saveEngine=model_int8.plan \
---minShapes=input:1 \
---optShapes=input:8 \
---maxShapes=input:8 \
---inputIOFormats=fp32:chw \
 --int8 \
 --profilingVerbosity=detailed \
 --builderOptimizationLevel=5
 
 trtexec --onnx=model.onnx \
 --saveEngine=model_best.plan \
---minShapes=input:1 \
---optShapes=input:8 \
---maxShapes=input:8 \
---inputIOFormats=fp32:chw \
 --best \
 --profilingVerbosity=detailed \
 --builderOptimizationLevel=5
